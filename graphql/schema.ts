b29637f80d0733c8be2c1e279ef9aa30b90871d0
rlycrvns <rlycrvns@gmail.com>
2021-11-04 11:56:05 -0700

    add: nexus

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,18 +1,21 @@
-import { gql } from "apollo-server-micro";
+import { makeSchema } from "nexus";
+import { join } from "path";
+import * as types from './types';
 
-export const typeDefs = gql`
-  type Link {
-    category: String
-    description: String
-    id: String
-    imageUrl: String
-    index: Int
-    title: String
-    url: String
-    userId: Int
-  }
-
-  type Query {
-    links: [Link]!
-  }
-`;
+export const schema = makeSchema({
+  types,
+  outputs: {
+    typegen: join(
+      process.cwd(),
+      'node_modules',
+      '@types',
+      'nexus-typegen',
+      'index.d.ts'
+    ),
+    schema: join(process.cwd(), 'graphql', 'schema.graphql'),
+  },
+  contextType: {
+    export: 'Context',
+    module: join(process.cwd(), 'graphql', 'context.ts'),
+  },
+});
